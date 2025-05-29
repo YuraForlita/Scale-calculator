d2cfa94e5beac775452cb5c35bcfbeff5e11c947
YuraForlita <forlitaura@gmail.com>
2025-05-29 12:55:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,19 @@ imageInput.addEventListener("change", (event) => {
 
   const img = new Image();
   img.onload = () => {
-    // Очистити полотно
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Білий фон
     ctx.fillStyle = "#ffffff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Обчислити масштаб до 1000x1000
     const maxSize = 1100;
     let scale = Math.min(maxSize / img.width, maxSize / img.height);
     const newWidth = img.width * scale;
     const newHeight = img.height * scale;
 
-    // Обчислити координати для центрування
     const x = (canvas.width - newWidth) / 2;
     const y = (canvas.height - newHeight) / 2;
 
-    // Намалювати зображення
     ctx.drawImage(img, x, y, newWidth, newHeight);
 
     downloadBtn.style.display = "inline-block";
@@ -36,7 +31,7 @@ imageInput.addEventListener("change", (event) => {
 
 downloadBtn.addEventListener("click", () => {
   const link = document.createElement("a");
-  link.download = "image_1200x1200.png";
-  link.href = canvas.toDataURL("image/png");
+  link.download = "image_1200x1200.jpg";
+  link.href = canvas.toDataURL("image/jpeg", 1.0);
   link.click();
 });
